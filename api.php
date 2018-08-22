3aaea25b01b4ebda670739ca9865276b1cf03b87
Dandy Raka <dandyraka13@gmail.com>
2018-08-22 20:49:28 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,32 @@
+<?php
+
+//CONFIG
+$CODE = "SGB-CODE";
+$KEY = "API-KEY";
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.sharinggilsblog.org/",
+  CURLOPT_HEADER => 0,
+  CURLOPT_VERBOSE => 1,
+  CURLOPT_AUTOREFERER => false,
+  CURLOPT_REFERER => "http://".$_SERVER['HTTP_HOST'],
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "code=".$CODE."&key=".$KEY
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
